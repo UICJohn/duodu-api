84d77754bf432483547d837db080b1c2cbec8462
John <johntang1026@gmail.com>
2019-05-07 11:48:11 +0930

    tweak error code

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -17,7 +17,7 @@ class ApplicationController < ActionController::API
     end
   end
 
-  def error!(msg, code = 500)
+  def error!(msg, code = 200)
     self.response_body = msg.to_json
     self.status = code
   end

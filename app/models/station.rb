0e1bfb69b97214b2e75700bef1c7d47026c0b7ab
john <johntang1026@gmail.com>
2019-10-05 21:43:38 +0930

    tweak

--- app/models/station.rb
+++ app/models/station.rb
@@ -4,7 +4,6 @@ class Station < ApplicationRecord
   has_and_belongs_to_many :subways
   has_one  :location, as: :target, dependent: :destroy
 
-
   validates :source_id, presence: true, uniqueness: true
 
 end
--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -1,11 +1,11 @@
 set :port, 22
 # set :pty,             true
-set :user, 'ubuntu'
+set :user, 'deploy'
 set :deploy_via, :remote_cache
 set :use_sudo, false
-set :branch, 'master'
+set :branch, 'develop'
 
-server '129.28.32.202',
+server '157.245.133.84',
   roles: [:web, :app, :db, :sidekiq],
   port: fetch(:port),
   user: fetch(:user),
@@ -16,7 +16,7 @@ set :conditionally_migrate, true
 
 set :deploy_to,       "/home/#{fetch(:user)}/#{fetch(:application)}"
 
-set :puma_user, 'ubuntu'
+set :puma_user, 'deploy'
 set :ssh_options, {
   forward_agent: true,
   auth_methods: %w(publickey),

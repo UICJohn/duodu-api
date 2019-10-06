b623e9af7ac1b5495249e7e6c92516eba875ee52
john <johntang1026@gmail.com>
2019-10-07 01:50:15 +1030

    tweak elasticsearch

--- config/initializers/elasticsearch.rb
+++ config/initializers/elasticsearch.rb
@@ -1,7 +1,7 @@
 if Rails.env.production?
   ENV["ELASTICSEARCH_URL"] = "http://192.168.3.1:9200"
 elsif Rails.env.staging?
-  ENV["ELASTICSEARCH_URL"] = "http://192.168.3.4:9200"
+  ENV["ELASTICSEARCH_URL"] = "http://localhost:9200"
 else
   ENV["ELASTICSEARCH_URL"] = "http://localhost:9200"
 end

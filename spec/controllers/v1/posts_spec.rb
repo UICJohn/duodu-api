6398bf32ec964882fc8ab6c7f6056813f2042212
john <johntang1026@gmail.com>
2019-10-09 17:21:08 +1030

    add test cases

--- spec/controllers/v1/posts_spec.rb
+++ spec/controllers/v1/posts_spec.rb
@@ -139,22 +139,9 @@ RSpec.describe 'Post', :type => :request do
     end
 
     it 'should filter condition' do
-      country  = create :country, name: "中国"
-      province = create :province, name: '江苏省', parent_id: country.id
-      city = create :city, name: '南京市', parent_id: province.id
-      suburb1 = create :suburb, name: "浦口区", parent_id: city.id
-      suburb2 = create :suburb, name: "秦淮区", parent_id: city.id
-      post = build :takehouse, user: (create :wechat_user, gender: 'female'), has_appliance: true, has_furniture: false, has_cook_top: true
-      post.location = build :location, city: city, suburb: suburb1, province: province
-      post.save
-
-      post = build :takehouse, user: (create :wechat_user, gender: 'female'), has_appliance: true, has_furniture: true, has_cook_top: false
-      post.location = build :location, city: city, suburb: suburb1, province: province
-      post.save
-
-      post = build :takehouse, user: (create :wechat_user, gender: 'female'), has_appliance: true, has_furniture: true, has_cook_top: true
-      post.location = build :location, city: city, suburb: suburb1, province: province
-      post.save
+      create :takehouse, user: (create :wechat_user, gender: 'female'), has_appliance: true, has_furniture: false, has_cook_top: true
+      create :takehouse, user: (create :wechat_user, gender: 'male'), has_appliance: true, has_furniture: true, has_cook_top: false
+      create :takehouse, user: (create :wechat_user, gender: 'female'), has_appliance: true, has_furniture: true, has_cook_top: true
 
       get "/v1/posts", params: {filters: {has_furniture: true, has_cook_top: true, has_appliance: true}}, headers: @headers
       expect(response).to be_successful
@@ -177,6 +164,76 @@ RSpec.describe 'Post', :type => :request do
       expect(posts.count).to eq 2
     end
 
+    it 'should filter livings' do
+      create :takehouse, user: (create :wechat_user, gender: 'female'), livings: 1, rooms: 2, toilets: 2
+      create :takehouse, user: (create :wechat_user, gender: 'male'), livings: 2, rooms: 2, toilets: 1
+      create :sharehouse, user: (create :wechat_user, gender: 'female'), livings: 2, rooms: 3, toilets: 2
+      get "/v1/posts", params: { filters: {livings: [2]} }, headers: @headers
+
+      expect(response).to be_successful
+      posts = JSON.load(response.body)["posts"]
+      expect(posts.count).to eq 2
+
+      get "/v1/posts", params: { filters: {livings: [1, 2]} }, headers: @headers
+
+      expect(response).to be_successful
+      posts = JSON.load(response.body)["posts"]
+      expect(posts.count).to eq 3
+
+      get "/v1/posts", params: { filters: {livings: [3]} }, headers: @headers
+
+      expect(response).to be_successful
+      posts = JSON.load(response.body)["posts"]
+      expect(posts.count).to eq 0
+    end
+
+    it 'should filter rooms' do
+      create :takehouse, user: (create :wechat_user, gender: 'female'), livings: 1, rooms: 2, toilets: 2
+      create :takehouse, user: (create :wechat_user, gender: 'male'), livings: 2, rooms: 2, toilets: 1
+      create :sharehouse, user: (create :wechat_user, gender: 'female'), livings: 2, rooms: 3, toilets: 2
+
+      get "/v1/posts", params: { filters: {rooms: [2] }}, headers: @headers
+      expect(response).to be_successful
+      posts = JSON.load(response.body)["posts"]
+      expect(posts.count).to eq 2
+
+      get "/v1/posts", params: { filters: {rooms: [3] }}, headers: @headers
+      expect(response).to be_successful
+      posts = JSON.load(response.body)["posts"]
+      expect(posts.count).to eq 1
+
+      get "/v1/posts", params: { filters: {rooms: [1, 3] }}, headers: @headers
+      expect(response).to be_successful
+      posts = JSON.load(response.body)["posts"]
+      expect(posts.count).to eq 2
+
+      get "/v1/posts", params: { filters: {rooms: [4, 3] }}, headers: @headers
+      expect(response).to be_successful
+      posts = JSON.load(response.body)["posts"]
+      expect(posts.count).to eq 1
+    end
+
+
+    it 'should filter toilets' do
+      create :takehouse, user: (create :wechat_user, gender: 'female'), livings: 1, rooms: 2, toilets: 2
+      create :takehouse, user: (create :wechat_user, gender: 'male'), livings: 2, rooms: 2, toilets: 1
+      create :sharehouse, user: (create :wechat_user, gender: 'female'), livings: 2, rooms: 3, toilets: 2
+
+      get "/v1/posts", params: { filters: {toilets: [2] }}, headers: @headers
+      expect(response).to be_successful
+      posts = JSON.load(response.body)["posts"]
+      expect(posts.count).to eq 2
+
+      get "/v1/posts", params: { filters: {toilets: [3] }}, headers: @headers
+      expect(response).to be_successful
+      posts = JSON.load(response.body)["posts"]
+      expect(posts.count).to eq 0
+
+      get "/v1/posts", params: { filters: {toilets: [1, 3] }}, headers: @headers
+      expect(response).to be_successful
+      posts = JSON.load(response.body)["posts"]
+      expect(posts.count).to eq 1
+    end
   end
 
 
